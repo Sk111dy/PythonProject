fee128a2c4a1a6337bfabd12b58158c704a2e557
IT Step <itstep@gmail.com>
2026-01-03 18:09:30 +0200

    les 3

--- hw8.py
+++ hw8.py
@@ -10,15 +10,13 @@ books = soup.select("article.product_pod")[:8]
 results = []
 
 for book in books:
-    # Назва книги
     title = book.h3.a["title"]
 
-    # Ціна
     price = book.select_one("p.price_color").text.strip()
 
-    # Рейтинг у вигляді слова (One, Two, Three...)
+
     rating_class = book.select_one("p.star-rating")["class"]
-    # Другий клас — це рейтинг
+
     rating = [c for c in rating_class if c != "star-rating"][0]
 
     results.append({
@@ -27,6 +25,6 @@ for book in books:
         "rating": rating
     })
 
-# Виводимо результат
+
 for i, book in enumerate(results, start=1):
     print(f"{i}. {book['title']} — {book['price']} — Rating: {book['rating']}")
